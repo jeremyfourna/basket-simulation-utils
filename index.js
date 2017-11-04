c34f5245d4eb49d2d43d63481d529113ddba4a96
jeremyfourna <jeremyfourna@gmail.com>
2017-11-04 17:29:38 +0100

    Fix eslint error

--- index.js
+++ index.js
@@ -7,9 +7,7 @@ function generateId() {
 }
 
 function generateName(listsToCompose) {
-  return R.join(' ', R.map(cur => {
-    return R.nth(Math.trunc(R.multiply(Math.random(), R.length(cur))), cur);
-  }, listsToCompose));
+  return R.join(' ', R.map(cur => R.nth(Math.trunc(R.multiply(Math.random(), R.length(cur))), cur), listsToCompose));
 }
 
 exports.generateId = generateId;
