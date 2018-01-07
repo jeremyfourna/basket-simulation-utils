54a62a7c2ffb587bc30a115bf5f1332aa067b7cf
jeremyfourna <jeremyfourna@gmail.com>
2018-01-07 15:09:24 +0100

    Update README and documentation

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # basket-simulation-utils
+
+[![NPM](https://nodei.co/npm/basket-simulation-utils.png)](https://www.npmjs.com/package/basket-simulation-utils)
+
+## Installation
+
+`npm i basket-simulation-utils`
+
+## Usage
+
+```js
+const {
+	generateId,
+	generateName
+} = require('basket-simulation-utils');
+
+const randomId = generateId(); // Eg: '911140631528115500'
+
+const firstNames = ['Brian', 'James'];
+const lastNames = ['Doe', 'Allen'];
+
+const randomName = generateName([firstNames, lastNames]); // Eg: 'James Allen'
+```
--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const R = require('ramda');
 
+// generateId :: a -> string
 function generateId() {
-  const id1 = Math.round(Math.random() * 1000000000);
-  const id2 = Math.round(Math.random() * 1000000000);
+  const idLength = 1000000000;
+  const id1 = Math.round(Math.random() * idLength);
+  const id2 = Math.round(Math.random() * idLength);
+
   return Number(R.join('', [id1, id2]));
 }
 
+// generateName :: [array] -> string
 function generateName(listsToCompose) {
-  return R.join(' ', R.map(cur => R.nth(Math.trunc(R.multiply(Math.random(), R.length(cur))), cur), listsToCompose));
+  return R.join(' ', R.map(cur => {
+    const randomPositionInList = Math.trunc(R.multiply(Math.random(), R.length(cur)));
+    return R.nth(randomPositionInList, cur);
+  }, listsToCompose));
 }
 
 exports.generateId = generateId;
